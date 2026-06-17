2dd1809db15b6a3c7e9c41d833a401f1df3ab916
Justin Dodge <jmdodge@gmail.com>
2026-06-17 14:44:25 -0700

    - New sub anchor: propeller - Misc prefab cleanup

--- Assets/Submachina/Scripts/Submarine/SubAnchors/SubAnchors.Hull.cs
+++ Assets/Submachina/Scripts/Submarine/SubAnchors/SubAnchors.Hull.cs
@@ -10,5 +10,6 @@ namespace Submachina.Core
         public static readonly AnchorId Tail = new(HullCat, 2);
         public static readonly AnchorId Top = new(HullCat, 3);
         public static readonly AnchorId Bottom = new(HullCat, 4);
+        public static readonly AnchorId Propeller = new(HullCat, 5);
     }
 }
